cb7bea0e1880297a43b01e31537fb5dca92f98ee
Liokang <60290160+Liokang@users.noreply.github.com>
2025-07-21 01:01:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 app.use(cors(
   {
-    origin:{'https://deploy-mern-1whq.vercel.app'},
+    origin:{'https://deploybackend-flame.vercel.app'},
     methods: {"POST",'GET'},
     credentials: true
   }
